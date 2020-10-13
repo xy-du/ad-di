1ccbd50d3153bd725c38b585f712aa541b8d5372
xyDu <xy_du@qq.com>
2020-10-13 11:16:13 +0800

    using environment property

--- src/main/java/dxy/springframework/addi/config/PropertyConfig.java
+++ src/main/java/dxy/springframework/addi/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package dxy.springframework.addi.config;
 
 import dxy.springframework.addi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * @author AD
@@ -14,6 +16,9 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment env;
+
     @Value("${dxy.username}")
     String userName;
     @Value("${dxy.password}")
@@ -21,9 +26,17 @@ public class PropertyConfig {
     @Value("${dxy.dburl}")
     String url;
 
+    /**
+     * if the environment property name is DXY_USERNAME, it will overwrite the property set here
+     * if the env property name is, say, USERNAME, then you have to get it specifically and manually using
+     * env.getProperty("propertyName")
+     * @return
+     */
     @Bean
     public FakeDataSource fakeDataSource(){
-        return new FakeDataSource(userName,password,url);
+        String env_username=env.getProperty("USERNAME");
+        return new FakeDataSource(env_username,password,url);
+//        return new FakeDataSource(userName,password,url);
     }
 
     @Bean
